495e0ac4c79b74637f7d3d95d7bd409ddcdda5a1
samaamro20 <samaamro20@gmail.com>
2018-11-13 14:13:03 +0200

    addtodo-fix #2

--- test.js
+++ test.js
@@ -1,12 +1,51 @@
-var test = require('tape');
+var tape = require('tape');
 var logic = require('./logic');
 
-test('Example test', function (t) {
+var testArr=[
+  {
+    id: 0,
+    description: 'smash avocados',
+    done: true,
+  },
+  {
+    id: 1,
+    description: 'make coffee',
+    done: false,
+  },
+];
+
+tape('Example test', function (t) {
     t.pass();
     t.end();
 });
 
 // Do addTodo tests here
+tape(' Add New toDo to Todos List ', function(t) {
+	var obj=  {
+    id: 2,
+    description: ' avocados',
+    done: false,
+  };
+    t.deepEqual(logic.addTodo(testArr, obj), [
+  {
+    id: 0,
+    description: 'smash avocados',
+    done: true,
+  },
+  {
+    id: 1,
+    description: 'make coffee',
+    done: false,
+  },
+  {
+    id: 2,
+    description: ' avocados',
+    done: false,
+  }
+],
+      "should returns array with a new todo appended");
+      t.end();
+})
 
 
 
